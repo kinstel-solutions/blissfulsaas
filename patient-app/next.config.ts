3365ff9936e6b6b523bada2f40fb38b82f1efab5
dethrtrns <rajshekharsingh060593@gmail.com>
2026-05-15 15:41:57 +0530

    lockfile root fixed to explicit path of each app

--- patient-app/next.config.ts
+++ patient-app/next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.resolve(__dirname, ".."),
+  },
   allowedDevOrigins: ['localhost', '127.0.0.1', '192.168.1.36', '192.168.1.34', '192.168.1.40'],
   images: {
     remotePatterns: [
